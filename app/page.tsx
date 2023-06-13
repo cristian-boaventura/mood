f35f201167461163da46528b6cb814ad1b7afaba
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-06-13 12:49:00 -0300

    centered home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-black flex justify-center text-white">
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
-        <h1 className="text-6xl">The best Journal app, period.</h1>
+        <h1 className="text-6xl mb-4">The best Journal app, period.</h1>
         <p className="text-2xl text-white/60 mb-4">
           This is the best app for tracking your mood through out your life. All
           you have to do is be honest.
